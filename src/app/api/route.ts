b0bcc0ce419b0d6438b029009fb0cc8a7fcb98cf
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-03 00:34:32 +0200

    Conenct api with database

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,25 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 export async function GET() {
-  return NextResponse.json({name: "Hello"})
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
+
+export async function POST(req: any) {
+  const data = await req.json();
+  const name = data.name;
+  const email = data.email
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  
+  return NextResponse.json(newUser);
+}
+
+
