95e3cf7a9f7a29343354a7cbea661ae8c1b5e031
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-04 14:02:07 +0200

    Create list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import List from "@/components/list";
+
 export default function Home() {
   return (
     <main>
       <h1>Travelo</h1>
+      <List />
     </main>
   );
 }
---
+++ src/components/list.tsx
@@ -0,0 +1,7 @@
+export default function List() {
+  return (
+    <section>
+      <h1>List</h1>
+    </section>
+  );
+}
