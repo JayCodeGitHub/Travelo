c5385f4454bfe99fee31a7e2395813d420b9a30d
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-13 00:21:45 +0200

    Move navbar to layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,8 @@ import NavBar from "@/components/navbar";
 
 export default function Contact() {
   return (
-    <>
-      <NavBar />
-      <main className="h-screen pt-20 text-white bg-gray-900">
-        <h1>Contact</h1>
-      </main>
-    </>
+    <main className="h-screen pt-20 text-white bg-gray-900">
+      <h1>Contact</h1>
+    </main>
   );
 }
--- src/app/offer/page.tsx
+++ src/app/offer/page.tsx
@@ -2,11 +2,8 @@ import NavBar from "@/components/navbar";
 
 export default function Offer() {
   return (
-    <>
-      <NavBar />
-      <main className="h-screen pt-20 text-white bg-gray-900">
-        <h1>Offer</h1>
-      </main>
-    </>
+    <main className="h-screen pt-20 text-white bg-gray-900">
+      <h1>Offer</h1>
+    </main>
   );
 }
