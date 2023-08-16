e25145fef7db5f997f4b2c7b7caed082a0ddaa0a
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-16 15:20:06 +0200

    Add header to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <header>
+    <div className="relative h-screen">
       <Image
         className="w-full"
         src="/images/hero.jpg"
@@ -10,7 +10,21 @@ export default function Hero() {
         width={1522}
         height={1005}
       />
-      <h1>Travelo</h1>
-    </header>
+      <div className="absolute flex justify-between w-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 px-52 h-4/6">
+        <header className="flex flex-col items-start justify-center h-full max-w-lg gap-8 ">
+          <h1 className="font-bold ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button>See Offer</button>
+        </header>
+        <div className="bg-red-500  w-96">Round Map</div>
+      </div>
+    </div>
   );
 }
