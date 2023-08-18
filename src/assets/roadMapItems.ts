0864a073082e36623650160780e250bbe1814dfe
JayCode Developer <JayCode.contakt@gmail.com>
2023-08-18 23:10:26 +0200

    Add items to roadMap

--- src/assets/roadMapItems.ts
+++ src/assets/roadMapItems.ts
@@ -1,6 +1,22 @@
 export const RoadMapItems = [
-    {},
-    {},
-    {},
-    {},
+     {
+        icon: "/icons/search.svg",
+        title: "Browse Destinations",
+        description: "Explore available destinations"
+    },
+    {
+        icon: "/icons/calendar.svg",
+        title: "Select Dates",
+        description: "Choose the dates for your vacation."
+    },
+    {
+        icon: "/icons/list.svg",
+        title: "View Activities",
+        description: "Discover exciting things to do."
+    },
+    {
+        icon: "/icons/check.svg",
+        title: "Make Reservation",
+        description: "Book preferred accommodation."
+    }
 ]
--- src/components/roadMap.tsx
+++ src/components/roadMap.tsx
@@ -5,20 +5,20 @@ import { RoadMapItems } from "@/assets/roadMapItems";
 export default function RoadMap() {
   return (
     <div className="rounded-md w-96 backdrop-blur-[3px] bg-white/10 flex justify-between py-12 px-4 flex-col">
-      <h2 className="ml-6 text-xl">RoudMap</h2>
-      {RoadMapItems.map(() => (
+      <h2 className="ml-6 text-xl">RoadMap</h2>
+      {RoadMapItems.map(({ icon, title, description }) => (
         <>
           <div className="flex w-full h-24 gap-4 px-2 py-2 rounded-md bg-black/60">
             <Image
-              className="object-cover w-auto h-full rounded-full aspect-square"
-              src="/images/hero.jpg"
+              className="w-auto h-full p-4"
+              src={icon}
               alt="Hero Image"
-              width={1522}
-              height={1005}
+              width={32}
+              height={32}
             />
             <div className="flex flex-col items-start justify-center gap-1">
-              <h3 className="font-bold">Lorem Ipsum</h3>
-              <p className="text-slate-200">Lorem ipsum dolor sit amet</p>
+              <h3 className="font-bold">{title}</h3>
+              <p className="text-sm text-slate-200">{description}</p>
             </div>
           </div>
         </>
